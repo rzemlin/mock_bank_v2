91cd2650dea6e4e134de317ec6949ec42a70846e
Ryan Zemlin <rzemlin@gmail.com>
2023-09-25 12:32:48 -0700

    add npm and remove yarn

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,14 @@
 # Be sure to restart your server when you modify this file.
+Rails.application.config.assets.precompile += %w(application.js)
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
+Rails.application.config.assets.paths += [
+    Rails.root.join('public', 'dist' ).to_s
+  ]
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
