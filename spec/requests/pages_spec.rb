91fc055f9c58992d2c03dbd33567671a09c3ba82
Ryan Zemlin <rzemlin@gmail.com>
2023-09-26 17:40:43 -0700

    add rspec tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /" do
-    context "when signed in" do 
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
-      expect (response.body).to include ("test")
+    context "when signed in" do
+      let(:user) { users(:test) }
+      before { sign_in(user) }
+
+      it "returns http success" do
+        get "/"
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include("Test")
+      end
+    end
+
+    context "when signed out" do
+      it "redirects to sign in" do
+        get "/"
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-  context "when signed out" do 
-    it "redirects to sign in" do
-      get "/"
-      expect(response).to redirect_to(new_user_session_path)
-      expect (response.body).to include ("test")
-    end
-  end
-
-
- end
 end
